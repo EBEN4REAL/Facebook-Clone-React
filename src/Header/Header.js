e2c17953679fecb879bd5f5e0b8ad406eb179adb
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-08-29 01:47:36 +0100

    initial commit

---
+++ src/Header/Header.js
@@ -0,0 +1,21 @@
+import React  from 'react'
+import './Header.css';
+import Logo from '../img/Facebook_f_logo_(2019).svg';
+
+const Header =  () =>  {
+    return (
+        <div className="header">
+            <div class="header__Left">
+                <img src={Logo} />
+            </div>
+            <div class="header__middle">
+                
+            </div>
+            <div class="header__right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsPOS_mPnRTnk6l0XgBdr3YzsgZDZb_ec",
+    authDomain: "facebook-clone-4969b.firebaseapp.com",
+    databaseURL: "https://facebook-clone-4969b.firebaseio.com",
+    projectId: "facebook-clone-4969b",
+    storageBucket: "facebook-clone-4969b.appspot.com",
+    messagingSenderId: "908333977754",
+    appId: "1:908333977754:web:a232069b44d1098ac30f23",
+    measurementId: "G-JFR7EHXME2"
+};
